38b9d5671b2e25fa326465051441f9d882a672a0
diegocardoso <diego.sacramento@engdb.com.br>
2023-11-09 10:51:33 -0300

    add: search candidate by name

--- applicant.racking.system/applicant.racking.system/src/main/java/com/api/applicant/racking/system/repositories/CandidateRepository.java
+++ applicant.racking.system/applicant.racking.system/src/main/java/com/api/applicant/racking/system/repositories/CandidateRepository.java
@@ -4,6 +4,10 @@ import com.api.applicant.racking.system.entities.CandidateEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CandidateRepository extends JpaRepository<CandidateEntity, Long> {
+
+    List<CandidateEntity> findByNameContainingIgnoreCase(String name);
 }
