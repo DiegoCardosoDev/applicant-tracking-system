6514abfd4f593f464b7804c0ae20811f730592fc
diegocardoso <diego.sacramento@engdb.com.br>
2023-11-09 19:10:58 -0300

    implements: adição de endpoints

--- applicant.racking.system/applicant.racking.system/src/main/java/com/api/applicant/racking/system/controllers/CandidateController.java
+++ applicant.racking.system/applicant.racking.system/src/main/java/com/api/applicant/racking/system/controllers/CandidateController.java
@@ -1,12 +1,52 @@
 package com.api.applicant.racking.system.controllers;
 
 
+import com.api.applicant.racking.system.dto.requests.CandidateRequest;
+import com.api.applicant.racking.system.dto.responses.CandidateDetailResponse;
+import com.api.applicant.racking.system.dto.responses.CandidateResponse;
+import com.api.applicant.racking.system.services.CandidateService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping (value = "/candidate", produces = {"application/json"})
 @RequiredArgsConstructor
 public class CandidateController {
+
+
+    private final CandidateService candidateDetailService;
+    private final CandidateService candidateService;
+
+
+    @PostMapping ("/save")
+    public ResponseEntity<CandidateResponse> createTechnologyStack(@RequestBody @Valid CandidateRequest candidateRequest) {
+        CandidateResponse createdTechnologyStack = candidateService.createCandidate(candidateRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdTechnologyStack);
+    }
+
+    @GetMapping("/list")
+    public List<CandidateResponse> getAllCandidatesWithStacks() {
+        return candidateService.getAllCandidatesWithStacks();
+    }
+
+
+    @GetMapping("/search/{id}")
+    public CandidateResponse getById(@PathVariable Long id) {
+        return candidateService.getCandidateById(id);
+    }
+
+    @GetMapping("/search")
+    public List<CandidateResponse> getCandidatesByName(@RequestParam String name) {
+        return candidateService.getCandidateByName(name);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteById(@PathVariable Long id) {
+        this.candidateService.deleteCandidate(id);
+    }
 }
