9a3df07304a1b1c0ec8c95ae7ece61f058a15f92
diegocardoso <diego.sacramento@engdb.com.br>
2023-11-13 09:29:54 -0300

    implements: método para procurar vagas por titulo e ativas

--- applicant.racking.system/applicant.racking.system/src/main/java/com/api/applicant/racking/system/repositories/JobsRepository.java
+++ applicant.racking.system/applicant.racking.system/src/main/java/com/api/applicant/racking/system/repositories/JobsRepository.java
@@ -2,8 +2,17 @@ package com.api.applicant.racking.system.repositories;
 
 import com.api.applicant.racking.system.entities.JobsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface JobsRepository extends JpaRepository<JobsEntity, Long> {
+
+    @Query ("SELECT j FROM JobsEntity j WHERE j.is_active = true")
+    List<JobsEntity> findActiveJobsIsTrue();
+
+    List<JobsEntity> findByJobTitleContainingIgnoreCase(String name);
+
 }
