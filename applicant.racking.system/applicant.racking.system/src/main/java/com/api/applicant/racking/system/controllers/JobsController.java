85a452e6e32e098b6c9a947c488f3ee33c272b1d
diegocardoso <diego.sacramento@engdb.com.br>
2023-11-13 09:40:10 -0300

    implements: controller para vagas.

--- applicant.racking.system/applicant.racking.system/src/main/java/com/api/applicant/racking/system/controllers/JobsController.java
+++ applicant.racking.system/applicant.racking.system/src/main/java/com/api/applicant/racking/system/controllers/JobsController.java
@@ -1,12 +1,43 @@
 package com.api.applicant.racking.system.controllers;
 
 
+import com.api.applicant.racking.system.dto.requests.JobsRequest;
+import com.api.applicant.racking.system.dto.responses.JobsResponse;
+import com.api.applicant.racking.system.services.JobsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping (value = "/jobs", produces = {"application/json"})
 @RequiredArgsConstructor
 public class JobsController {
+
+    private final JobsService jobsService;
+
+
+    @PostMapping ( "/save" )
+    public ResponseEntity<JobsResponse> createTechnologyStack(@RequestBody JobsRequest jobsRequest){
+
+        JobsResponse jobsResponse = jobsService.createNewJob(jobsRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(jobsResponse);
+    }
+
+    @GetMapping ( "/list" )
+    public List<JobsResponse> getAllJobs(){
+        return jobsService.getAllJobs();
+    }
+
+    @GetMapping ( "/active" )
+    public List<JobsResponse> getAllJobsActiveTrue(){
+        return jobsService.getAllActiveJobs();
+    }
+
+    @GetMapping ( "/title" )
+    public List<JobsResponse> getJobsByTitle(@RequestParam String title){
+        return jobsService.getJobByTitle(title);
+    }
 }
